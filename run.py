31e5811639e3c4166b6080ccefc1ee4d1f1c5b29
Leonardo-Cardoso-ie <leonardocardoso.ie@gmail.com>
2022-09-18 09:44:03 +0000

    create a game border

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+board = ["_", "_", "_",
+        "_", "_", "_",
+        "_", "_", "_" ]
+
+
+
+
+# print the tic toc toe game board
+
+#take player input
+
+#check for win or tie
+
+#switch the player
+
+#check for win or tie 2
